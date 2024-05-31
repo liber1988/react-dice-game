fc3503b541c5146fa52e683ec08fd30faa04ddf8
liber1988 <92869703+liber1988@users.noreply.github.com>
2024-05-31 10:34:10 +0300

    first push

---
+++ src/components/middle_container/middle_container.jsx
@@ -0,0 +1,11 @@
+  .middle-layer {
+    position: absolute;
+    transform: translate(-50%, 0);
+    left: 50%;
+    width: 10vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+  }
